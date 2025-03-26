aee5dda22c9b22ac3780f77b8f2a07c96325e8a9
Gustavo Giraldo <gustavoadolfogiraldorendon@gmail.com>
2025-03-25 23:07:14 -0500

    Added UserRepository

---
+++ src/main/java/com/gustavogiraldo/taskmanager/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.gustavogiraldo.taskmanager.user.repository;
+
+import com.gustavogiraldo.taskmanager.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
+}
