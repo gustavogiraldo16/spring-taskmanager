3c5aaaa1af7279694761a7cf3c15d9fbc0561143
Gustavo Giraldo <gustavoadolfogiraldorendon@gmail.com>
2025-03-26 10:33:41 -0500

    Added UserService

---
+++ src/main/java/com/gustavogiraldo/taskmanager/user/service/UserService.java
@@ -0,0 +1,36 @@
+package com.gustavogiraldo.taskmanager.user.service;
+
+import com.gustavogiraldo.taskmanager.user.entity.User;
+import com.gustavogiraldo.taskmanager.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(String id) {
+        return userRepository.findById(id);
+    }
+
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String id) {
+        userRepository.deleteById(id);
+    }
+}
